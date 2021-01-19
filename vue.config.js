ce290148da794b0337f0462043cc1b4f9d6c03aa
Ellie-dynamsoft <Ellie@dynamsoft.com>
2021-01-19 16:59:01 +0800

    dwt 16.2.0 in vue app

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	configureWebpack: {
+	    performance: {
+	    	hints:'warning',
+	    	maxEntrypointSize: 50000000,
+	    	maxAssetSize: 30000000,
+	    	assetFilter: function(assetFilename) {
+	    		return assetFilename.endsWith('.js');
+	    	}
+	    }
+    }
+
+}
